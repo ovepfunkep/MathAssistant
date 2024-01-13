e6c0cdef7c2e664190939f652a2ab7152a262355
Ivan_ <Ivan_@DESKTOP-RO7HB57>
2024-01-13 10:57:23 +0700

    Add project files.

---
+++ MathAssistant/Figures/Circle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathAssistant.Figures
+{
+    public class Circle : IFigure
+    {
+        public int Radius { get; set; }
+
+        public double GetSquare() => Math.PI * Math.Pow(Radius, 2);
+    }
+}
---
+++ MathAssistant/Figures/IFigure.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathAssistant.Figures
+{
+    internal interface IFigure
+    {
+        double GetSquare();
+    }
+}
